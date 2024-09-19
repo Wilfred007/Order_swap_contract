953ddc39e639682212c89762075b04557e07baab
Wilfred007 <adzerwilfred007@hmail.com>
2024-09-19 14:42:17 +0100

    swap contract

---
+++ contracts/Order_swap.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.24;
+
+
+Contract Order_swap {
+
+
+
+
+    constructor(){
+
+    }
+
+
+
+    function create_order() external {
+
+    }
+
+
+
+    function order_fufilled() external {
+        
+    }
+}
